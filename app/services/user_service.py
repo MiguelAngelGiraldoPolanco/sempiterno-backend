6f68fcce095f86932c0e9d3b8dc678ad838baa90
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-11 10:45:01 +0200

    fix: add try except to modificar_usuario

--- app/services/user_service.py
+++ app/services/user_service.py
@@ -33,26 +33,34 @@ def obtener_usuario_por_id(db: Session, user_id: int) -> Optional[User]:
 
 def obtener_usuario_por_email(db: Session, user_email: EmailStr) -> Optional[User]:
     sentencia = select(User).where(User.email == user_email)
+
     return db.exec(sentencia).first()
 
 
-def modificar_usuario(db: Session, user_data: User) -> User:
-    user_db = db.get(User, User.id)
+def modificar_usuario(db: Session, user_id: int, user_data: User) -> User:
+    user_db = db.get(User, user_id)
     if not user_db:
-        raise HTTPException(status_code=400, detail="Usuario no encontrado")
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
 
     user_db.email = user_data.email
     user_db.password_hash = user_data.password_hash
     user_db.update_at = datetime.now(timezone.utc)
 
     db.add(user_db)
-    db.commit()
-    db.refresh(user_db)
-    return user_db
+    try:
+        db.commit()
+        db.refresh(user_db)
+        return user_db
+    except Exception:
+        db.rollback()
+        raise HTTPException(
+            status_code=400,
+            detail="No se pudo actualizar: el email ya está en uso o hubo un error interno",
+        )
 
 
 def eliminar_usuario(db: Session, user_id: int) -> dict:
-    user_db = db.get(User, User.id)
+    user_db = db.get(User, user_id)
     if not user_db:
         raise HTTPException(status_code=404, detail="Usuario no encontrado")
     db.delete(user_db)
