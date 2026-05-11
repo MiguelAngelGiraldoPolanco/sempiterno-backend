4a2e1033cec71fb459edfc3027de157d7ff94a3e
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-11 13:24:10 +0200

    refactor: change List to list and add status code for post

--- app/api/v1/endpoints/client.py
+++ app/api/v1/endpoints/client.py
@@ -1,36 +1,43 @@
 # Archivo diseñado para recibir los correos de los posibles clientes pero aun no son usuarios de la tienda solo dejan sus correos para recibir descuerstos de los lanzamientos mensuales
 
 from datetime import datetime
-from typing import List
 
 from app.db import database
 from app.schemas import client
 from app.services import client_service
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from pydantic import EmailStr
 from sqlmodel import Session
 
-router = APIRouter(prefix="clients", tags=["Clients"])
+router = APIRouter(prefix="/clients", tags=["Clients"])
 
 
-@router.get("/", response_model=List[client.ClientRead])
+@router.get("/", response_model=list[client.ClientRead])
 def read_client(db: Session = Depends(database.get_session)):
     return client_service.obtener_todos_los_clients(db)
 
 
-@router.get("/reporte/fechas", response_model=List[client.ClientRead])
+@router.get("/reporte/fechas", response_model=list[client.ClientRead])
 def reporte_mensual(
     inicio: datetime, fin: datetime, db: Session = Depends(database.get_session)
 ):
     return client_service.obtener_clients_por_fecha(db, inicio, fin)
 
 
-@router.get("/buscar", response_model=client.ClientRead)
-def read_client_email(email: EmailStr, db: Session = Depends(database.get_session)):
-    return client_service.obtener_clients_por_email(db, email)
+@router.get("/email/{email_cliente}", response_model=client.ClientRead)
+def read_client_email(
+    email_cliente: EmailStr, db: Session = Depends(database.get_session)
+):
+    client = client_service.obtener_clients_por_email(db, email_cliente)
+    if not client:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Cliente no encontrado",
+        )
+    return client
 
 
-@router.post("/", response_model=client.ClientRead)
+@router.post("/", response_model=client.ClientRead, status_code=status.HTTP_201_CREATED)
 def crear_cliente(
     client_in: client.ClientCreate, db: Session = Depends(database.get_session)
 ):
