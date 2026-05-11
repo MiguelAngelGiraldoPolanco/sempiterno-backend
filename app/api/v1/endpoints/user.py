f2859ad0b3d3a59153dd3c4f7888f75a8fa876f8
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-11 10:55:20 +0200

    refactor: delete async and correct typo errors

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -2,43 +2,41 @@ from app.db import database
 from app.schemas import user
 from app.services import user_service
 from fastapi import APIRouter, Depends, HTTPException
-from pydantiuc import EmialStr
+from pydantic import EmailStr
 from sqlmodel import Session
 
 router = APIRouter()
 
 
 @router.get("/{id_user}", response_model=user.UserRead)
-async def obtener_usuario_por_id(
-    id_user: int, db: Session = Depends(database.get_session)
-):
+def obtener_usuario_por_id(id_user: int, db: Session = Depends(database.get_session)):
     return user_service.obtener_usuario_por_id(db, id_user)
 
 
-@router.get("/{email_user}", response_model=user.UserRead)
-async def obtener_usuario_por_emial(
-    email_user: EmialStr, db: Session = Depends(database.get_session)
+@router.get("/email/{email_user}", response_model=user.UserRead)
+def obtener_usuario_por_emial(
+    email_user: EmailStr, db: Session = Depends(database.get_session)
 ):
     user = user_service.obtener_usuario_por_email(db, email_user)
     if not user:
-        raise HTTPException(sstatus_code=404, detail="Usuario no encontrado")
-    return
+        raise HTTPException(status_code=404, detail="Usuario no encontrado")
+    return user
 
 
 @router.post("/", response_model=user.UserRead)
-async def crear_usuario(
+def crear_usuario(
     user_in: user.UserCreate, db: Session = Depends(database.get_session)
 ):
     return user_service.crear_usuario(db, user_in)
 
 
-@router.put("/", response_model=user.UserBase)
-async def modificar_usiuario(
-    user_in: user.UserBase, db: Session = Depends(database.get_session)
+@router.put("/{user_id}", response_model=user.UserBase)
+def modificar_usiuario(
+    user_in: user.UserBase, user_id: int, db: Session = Depends(database.get_session)
 ):
-    return user_service.modificar_usuario(db, user_in)
+    return user_service.modificar_usuario(db, user_id, user_in)
 
 
 @router.delete("/{id_user}")
-async def eliminar_usuario(id_user: int, db: Session = Depends(database.get_session)):
+def eliminar_usuario(id_user: int, db: Session = Depends(database.get_session)):
     return user_service.eliminar_usuario(db, id_user)
