e426255e10abff76b1a44bf39070bac4bc1b0fa3
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-12 12:24:21 +0200

    refactor: add lead endpoint

--- app/api/v1/api_v1.py
+++ app/api/v1/api_v1.py
@@ -1,4 +1,4 @@
-from app.api.v1.endpoints import client, ticket, user
+from app.api.v1.endpoints import lead, ticket, user
 from fastapi import APIRouter
 
 api_router = APIRouter()
@@ -6,6 +6,6 @@ api_router = APIRouter()
 # Aquí va cada módulo de la v1
 api_router.include_router(ticket.router)
 
-api_router.include_router(client.router)
+api_router.include_router(lead.router)
 
 api_router.include_router(user.router)
