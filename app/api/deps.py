decd2cf9bb1aae264fc6d38a5129c4b0384c072f
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-13 12:39:48 +0200

    create: verify tokens and admin

---
+++ app/api/deps.py
@@ -0,0 +1,55 @@
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+from sqlalchemy.orm import Session
+
+from app.core.config import settings
+from app.db import database
+from app.models.user import User
+
+# Esto le dice a FastAPI dónde buscar el token (en la ruta /login)
+reusable_oauth2 = OAuth2PasswordBearer(tokenUrl="/api/v1/users/login")
+
+
+def get_current_user(
+    db: Session = Depends(database.get_session),
+    token: str = Depends(reusable_oauth2),
+) -> User:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.SECRET_KEY,
+            algorithms=[settings.ALGORITHM],
+        )
+        user_id: str = payload.get("sub")
+        if user_id is None:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Token inválido",
+            )
+
+    except JWTError:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="No se pudo validar el token",
+        )
+
+    user = db.query(User).filter(User.id == user_id).first()
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Usuario no encontrado",
+        )
+
+    return user
+
+
+def get_current_admin_user(
+    current_user: User = Depends(get_current_user),
+):
+    if not current_user.is_admin:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="El usuario no tiene privilegios de Admin",
+        )
+    return current_user
