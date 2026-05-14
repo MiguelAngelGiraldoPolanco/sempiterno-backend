a24d33cdb9c8cf3b4bd168ae1ad4e4a7999a48b4
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-14 14:47:20 +0200

    style: change to do test

--- app/api/resend.py
+++ app/api/resend.py
@@ -7,13 +7,13 @@ resend.api_key = settings.RESEND_APY_KEY
 
 def email_verify(email_destino: str, lead_name: str, token: str):
     params = {
-        "from": "Velas Magia <hola@tu-dominio.com>",
+        "from": "Velas Magia <onboarding@resend.dev>",
         "to": [email_destino],
         "subject": "🕯️ Activa tu cupón de bienvenida",
         "html": f"""
             <h1>¡Hola! {lead_name}</h1>
             <p>Para recibir tu cupón del 10%, por favor confirma que este es tu correo:</p>
-            <a href="https://tu-api.com/verify?email={email_destino}?token={token}" 
+            <a href="http://localhost:8000/api/v1/email_verify/verify?email={email_destino}&token={token}" 
                style="background: #000; color: #fff; padding: 10px 20px;">
                Confirmar y Ver Cupón
             </a>
