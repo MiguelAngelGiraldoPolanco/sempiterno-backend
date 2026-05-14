4a0e4c0a9f994f91ceaf478ce16ac5b8cf20af28
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-14 16:02:25 +0200

    fix: add user to do test

--- app/db/database.py
+++ app/db/database.py
@@ -1,10 +1,27 @@
 from typing import Generator
 
 from app.core.config import settings
+from app.core.security import get_password_hash
+from app.models.user import User
 from sqlmodel import Session, SQLModel, create_engine
 
 # El motor: "check_same_thread" es necesario solo para SQLite
 engine = create_engine(settings.DATABASE_URL, echo=True)
+
+
+# creamos un usuario para pruebas en sqlite
+def create_user():
+    with Session(engine) as session:
+        admin = User(
+            email="admin@admin.com",
+            password_hash=get_password_hash("admin123"),
+            is_admin=True,
+        )
+        session.add(admin)
+        session.commit()
+        session.refresh(admin)
+
+        print("Created admin:", admin)
 
 
 # Función para crear las tablas al iniciar la app
