4899e9c2ab31f3768b0173138fcb3e6a2b022e3a
Miguel Angel Giraldo Polanco <miguelgiraldo1116@gmail.com>
2026-05-14 14:48:57 +0200

    create: add all relationships tables

---
+++ app/models/db_model.py
@@ -0,0 +1,55 @@
+from datetime import datetime, timezone
+from typing import Any, List, Optional
+
+from pydantic import EmailStr
+from sqlmodel import JSON, Column, Field, Relationship, SQLModel
+
+
+class Coupon(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(unique=True, index=True)
+    create_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    finish_at: datetime | None = Field(default=None)
+    discount: float = 0.0
+
+    tickets: list["Ticket"] = Relationship(back_populates="coupon")
+
+    leads: list["Lead"] = Relationship(back_populates="coupon")
+    # Esto es CRUCIAL en Pydantic v2 / SQLModel moderno
+
+
+class Lead(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    email: EmailStr = Field(unique=True, index=True)
+    create_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    marketing_consent: bool = False
+    is_verify: bool = False
+    verification_token: Optional[str] = Field(default=None)
+
+    tickets: list["Ticket"] = Relationship(back_populates="lead")
+
+    coupon_id: Optional[int] = Field(default=None, foreign_key="coupon.id")
+
+    coupon: Coupon | None = Relationship(back_populates="leads")
+    # Esto es CRUCIAL en Pydantic v2 / SQLModel moderno
+
+
+class Ticket(SQLModel, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+    customerName: str = Field(index=True)
+    # Cambiamos List[dict] por List[Any] o simplemente Any para el tipo de Python
+    # y nos aseguramos de que Pydantic permita modelos anidados
+    products: List[Any] = Field(default=[], sa_column=Column(JSON))
+    create_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    total: float
+    iva: float = 0.0
+
+    lead_id: Optional[int] = Field(default=None, foreign_key="lead.id")
+    coupon_id: Optional[int] = Field(default=None, foreign_key="coupon.id")
+
+    coupon: Coupon | None = Relationship(back_populates="tickets")
+
+    lead: Lead | None = Relationship(back_populates="tickets")
+
+    # Esto es CRUCIAL en Pydantic v2 / SQLModel moderno
